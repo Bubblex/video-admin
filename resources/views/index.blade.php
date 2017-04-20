827891fce6198f424336aaba11edc2f644056ca9
江肖 <407505475@qq.com>
2017-04-20 22:26:12 +0800

    初始化项目

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>邮币卡大讲堂</title>
+  <link rel="stylesheet" href="/resource/app.css" />
+  <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+</head>
+<body>
+  <div id="root"></div>
+  <script src="/resource/app.js"></script>
+</body>
+</html>
